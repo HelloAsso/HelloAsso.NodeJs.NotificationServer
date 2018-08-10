0e28b299468ca447878c7ee58b6a0e10e6604878
Gil <gfelot@gmail.com>
2018-08-10 12:09:26 +0200

    Ajout du parser pour x-www-form-urlencoded

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
 const fastify = require('fastify')({
-  logger: true
-})
+    logger: true
+});
+const helmet = require('fastify-helmet');
 
+// Ajout de ce parser pour prendre en compte la réponse de type x-www-form-urlencoded
+fastify.register(require('fastify-formbody'));
 
+// Ajout de header de sécurité
+fastify.register(
+    helmet,
+    {hidePoweredBy: {}}
+);
 
 // Test de la mise en place de l'api
-fastify.get('/', async (request, reply) => {
-  reply.send('Je suis actif et prêt à recevoir des requêtes')
-})
+fastify.post('/', async (request, reply) => {
+    const {body} = request;
 
-// Réception des infos des campagnes
-fastify.post('/campaigns', async (request, reply) => {
+    console.log(body);
+    reply.code(204);
+});
 
-  const { body } = request
-
-  console.log(body)
-
-  return body
-})
-
-// Réception des infos des paiements
-fastify.post('/payments', async (request, reply) => {
-
-  const { body } = request
-
-  console.log(body)
-
-  return body
-})
-
-// Run the server!
+// Démarrage du serveur.
 const start = async () => {
-  try {
-    const port = process.env.PORT || 3000
-    await fastify.listen(port, '0.0.0.0')
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+    try {
+        const port = process.env.PORT || 3000;
+        await fastify.listen(port, '0.0.0.0');
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
